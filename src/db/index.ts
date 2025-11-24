adb7bafa512e94766f2318c14e9d98e0f94e2138
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-11-24 19:56:23 +0900

    <Fix> BUILD ISS

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,30 +1,25 @@
 // src/db/index.ts
 import { Sequelize } from "sequelize";
-import { URL } from "url";
 import logger from "jet-logger";
 import ENV from "../common/constants/ENV";
 
 /**
  * Sequelize 인스턴스 생성 (MySQL 연결)
- * DATABASE_URL 형식 예:
- *   mysql://user:password@localhost:3306/webhw3
+ * 이제 DATABASE_URL이 아닌 개별 환경변수 기반으로 연결
  */
-const dbUrl = new URL(ENV.DatabaseUrl);
-
-// 과제 요구사항: DB 이름(webhw3) 통일
-const REQUIRED_DB_NAME = "webhw3";
-if (dbUrl.pathname.replace("/", "") !== REQUIRED_DB_NAME) {
-  dbUrl.pathname = `/${REQUIRED_DB_NAME}`;
-}
-
-export const sequelize = new Sequelize(dbUrl.toString(), {
-  dialect: "mysql",
-  logging: false,
-});
+export const sequelize = new Sequelize(
+  ENV.DbName, // database
+  ENV.DbUser, // username
+  ENV.DbPassword, // password
+  {
+    host: ENV.DbHost,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 /**
  * DB 연결 테스트 함수
- * 서버 시작 시 불러서 정상 연결 여부 확인 가능
  */
 export async function connectDB() {
   try {
